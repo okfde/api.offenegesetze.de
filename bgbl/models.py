79b1e9887bef45131eebf06e6d708583cc02460a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-08-15 21:38:01 +0200

    Update models and search index definition

--- bgbl/models.py
+++ bgbl/models.py
@@ -20,11 +20,15 @@ class Publication(models.Model):
 
 class PublicationEntry(models.Model):
     publication = models.ForeignKey(
-        Publication, on_delete=models.CASCADE
+        Publication, on_delete=models.CASCADE, related_name='entries'
     )
+    order = models.PositiveIntegerField()
     title = models.TextField(blank=True)
     law_date = models.DateField(null=True, blank=True)
     page = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('page',)
 
     def __str__(self):
         return self.title
