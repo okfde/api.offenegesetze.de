40f415c7a2ada69194231f8bd35175c2bff8b391
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-08-16 17:38:00 +0200

    Add logging config

--- offenegesetze/settings.py
+++ offenegesetze/settings.py
@@ -130,6 +130,49 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+DEFAULT_LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{server_time}] {message}',
+            'style': '{',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
+
 API_URL = 'https://api.offenegesetze.de'
 SITE_URL = 'https://offenegesetze.de'
 
